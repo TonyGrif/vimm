656658fef359a2469f3a66ad543e1b609cefa9e9
TonyGrif <TonyGriffin2000@gmail.com>
2024-11-27 16:11:57 -0500

    feat: add request function to the vault class

--- tests/vault/test_vault.py
+++ tests/vault/test_vault.py
@@ -10,3 +10,14 @@ class TestVault:
 
         with pytest.raises(ValueError) as _:
             Vault("Playstation 3")
+
+        assert Vault("PS2", validate=True, auto_scrape=True).entries is not None 
+
+    def test_scrape(self):
+        ps2 = Vault("PS2", auto_scrape=True)
+        assert ps2.entries is not None
+        assert len(ps2.entries) == 1807
+
+        assert "Hitman: Blood Money" in ps2.entries
+        assert "Devil May Cry 3: Dante's Awakening" in ps2.entries
+        assert "Sly 2: Band of Thieves" in ps2.entries
