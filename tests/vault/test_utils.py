6121d9c5bf796d1c418e2e3c4a43d7f67ade93fd
TonyGrif <TonyGriffin2000@gmail.com>
2024-11-27 15:45:02 -0500

    feat: scrape all tables of a console vault

--- tests/vault/test_utils.py
+++ tests/vault/test_utils.py
@@ -2,7 +2,12 @@ from vault.utils import request_letter, request_vault
 
 class TestUtils:
     def test_request_vault(self):
-        pass
+        res = request_vault("Atari2600")
+        assert len(res) == 478
+
+        assert "Pitfall! Pitfall Harry's Jungle Adventure" in res
+        assert "Missile Command" in res
+        assert "Space Invaders" in res
 
     def test_request_letter(self):
         res = request_letter("Atari2600", "A")
