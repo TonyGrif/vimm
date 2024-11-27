e1ef13bc3d4677d07b3d80d010c5e58274ca2e36
TonyGrif <TonyGriffin2000@gmail.com>
2024-11-26 19:46:30 -0500

    script: add script to gather available systems

---
+++ scripts/__init__.py
@@ -0,0 +1,7 @@
+"""This module defines scripts for developing the vimm.net scraper"""
+
+from .systems import get_available_systems
+
+__all__ = [
+    "get_available_systems",
+]
---
+++ vault/vault.py
@@ -0,0 +1,6 @@
+"""This module defines the vault class for scraping vimm.net/vault data."""
+
+
+class Vault:
+    def __init__(self) -> None:
+        pass
