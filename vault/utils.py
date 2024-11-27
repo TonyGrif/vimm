e73d1a9316d6746d8097b049cc71d6a70d55e2c0
TonyGrif <TonyGriffin2000@gmail.com>
2024-11-27 15:45:02 -0500

    feat: scrape all tables of a console vault

--- vault/utils.py
+++ vault/utils.py
@@ -1,5 +1,6 @@
 """This modules defines utility functions for the vault scraper"""
 
+from string import ascii_uppercase as letters
 from typing import List
 
 import httpx
@@ -8,13 +9,19 @@ from bs4 import BeautifulSoup
 URL = 'https://vimm.net/vault'
 
 
-def request_vault() -> None:
+def request_vault(console) -> List[str]:
     """Make a request on the full vault
 
     Args:
         console: The console to scrape
     """
-    return
+    result = []
+
+    result.extend(request_number(console))
+    for letter in letters:
+        result.extend(request_letter(console, letter))
+
+    return result
 
 
 def request_letter(console: str, character: str) -> List[str]:
@@ -25,11 +32,24 @@ def request_letter(console: str, character: str) -> List[str]:
         character: The letter to scrape
     """
     res = httpx.get(f'{URL}/{console}/{character}')
-
     soup = BeautifulSoup(res.text, 'html.parser')
+    return _parse_table(soup)
 
+
+def request_number(console: str) -> List[str]:
+    """Make a request on the # vault page; requires different URL than letters
+
+    Args:
+        console: The console to scrape
+    """
+    res = httpx.get(f'{URL}/?p=list&system={console}&section=number')
+    soup = BeautifulSoup(res.text, 'html.parser')
+    return _parse_table(soup)
+
+
+def _parse_table(soup: BeautifulSoup) -> List[str]:
+    """Parse the soup for games"""
     titles = []
     for title in soup.find_all('td', {'style': 'width:auto'}):
         titles.append(title.find('a').text)
-
     return titles
