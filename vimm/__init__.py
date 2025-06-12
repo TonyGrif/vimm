e611d0b265b093df5b39be46bb9f86b6557c5ac1
TonyGrif <TonyGriffin2000@gmail.com>
2025-06-12 18:43:09 -0400

    vault(platform): scrape available platforms from vault home page

---
+++ vimm/__init__.py
@@ -0,0 +1,5 @@
+"""This module contains the scrapers for vimm.net"""
+
+from .vault import AvailablePlatformScraper
+
+__all__ = ["AvailablePlatformScraper"]
---
+++ vimm/vault/__init__.py
@@ -0,0 +1,5 @@
+"""This module contains scrapers pertaining to Vimm's vault"""
+
+from .platforms import AvailablePlatformScraper
+
+__all__ = ["AvailablePlatformScraper"]
