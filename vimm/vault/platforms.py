2870be9407c5e6136b47454e9202d21ede347dca
TonyGrif <TonyGriffin2000@gmail.com>
2025-06-12 18:43:09 -0400

    vault(platform): scrape available platforms from vault home page

---
+++ vimm/vault/platforms.py
@@ -0,0 +1,83 @@
+"""Contains scraper pertaining to the available platforms on Vimm"""
+
+from dataclasses import dataclass
+from typing import List, Optional, no_type_check
+
+import httpx
+from bs4 import BeautifulSoup
+
+
+@dataclass
+class Platform:
+    """Information pertaining to the platform on Vimm"""
+
+    name: str
+    link: str
+    year: int
+    designation: str
+
+
+class AvailablePlatformScraper:
+    """Scrapes available platforms from Vimm's vault
+
+    Attributes:
+        platforms: all available platforms
+        consoles: the available home consoles
+        handhelds: the available handheld consoles
+    """
+
+    def __init__(self) -> None:
+        """Constructor for the PlatformScraper
+
+        Args:
+            TBW
+        """
+        self._platforms: Optional[List[Platform]] = None
+        self._consoles = None
+        self._handhelds = None
+
+    @property
+    def platforms(self) -> List[Platform]:
+        """Return all the available platform's on Vimm"""
+        if self._platforms is not None:
+            return self._platforms
+
+        res = self._make_request(verify=False)
+        self._platforms = self._parse_request(res)
+        return self._platforms
+
+    @property
+    def consoles(self) -> List[Platform]:
+        """Return all the home consoles available on Vimm"""
+        raise NotImplementedError
+
+    @property
+    def handhelds(self) -> List[Platform]:
+        """Return all the handheld platforms on Vimm"""
+        raise NotImplementedError
+
+    def _make_request(self, verify: bool = False) -> str:
+        res = httpx.get("https://vimm.net/vault", verify=verify)
+        res.raise_for_status()
+        return res.text
+
+    @no_type_check
+    def _parse_request(self, res: str) -> List[Platform]:
+        soup = BeautifulSoup(res, "html.parser")
+        tables = soup.find_all("table")
+        data = []
+
+        for table in tables:
+            entries = table.find_all("tr")
+            for entry in entries:
+                values = entry.find_all("td")
+                data.append(
+                    Platform(
+                        name=values[0].find("a").text,
+                        link=f"https://vimm.net{values[0].find('a')['href']}",
+                        year=values[1].text,
+                        designation=table.find("caption").text,
+                    )
+                )
+
+        return data
